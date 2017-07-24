78498241fd76cd4f65dcde52d395cae7c0615bfd
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-07-24 15:37:52 +0200

    Integrated new and create actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,34 @@
 class PortfoliosController < ApplicationController
+  # before_action :set_portfolio
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio is now live.'}
+      else
+        format.html { render :new}
+      end
+    end
+
+  end
+
+  private
+
+  # def set_portfolio
+  #   @portfolio_item = Portfolio.find[params[:id]]
+  # end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
