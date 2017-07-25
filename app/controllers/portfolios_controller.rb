33085cedea01bb1133388b20e6139cfc344001b7
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-07-25 08:17:47 +0200

    implement edit and update actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -22,6 +22,21 @@ class PortfoliosController < ApplicationController
 
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.'}
+      else
+        format.html { render :edit}
+      end
+    end
+  end
+
   private
 
   # def set_portfolio
